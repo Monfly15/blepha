4d78cf9a516740db894f93778d2e0522769192bd
Cristian Román-Palacios <cromanpa94@gmail.com>
2023-02-23 20:48:37 -0500

    Necesita trabajo adicional

--- moka.R
+++ moka.R
@@ -3,20 +3,24 @@ library(maps)
 library(phytools)
 library(mapdata)
 
-
 data <- read.csv("distribucion_ble.csv")[,1:3]
-#row.names(data) <- data$spp
-#data$spp  <- NULL
+data$spp[which(data$spp == "COL20_spn_ranulfoi" )] <- "COL020_spn_ranulfoi"
+
 tree <- read.nexus("blepha_final.nex")
+tree <- drop.tip(tree, "OUTGROUP")
 
-## Las especies que no existen en el arbol
-tree$tip.label[! tree$tip.label %in% data$spp]
+##Necesitas una matriz
+data2 <- cbind(data$lat, data$long )
+row.names(data2) <- data[,1]
 
-## Las especies que existen en el arbol
-tree$tip.label[tree$tip.label %in% data$spp]
+##Ahora usas la funcion
+##Sospecho que tienes que ajustar la region.
+##Revisa este link 
+##http://blog.phytools.org/2019/03/a-follow-up-comment-on-phylotomap-for.html
+obj <- phylo.to.map(tree, data2,plot=FALSE, rotate=FALSE,
+                    database="worldHires")
+plot(obj)
 
 
-tree$tip.label
-data$spp
-obj<-phylo.to.map(tree,data, plot=FALSE)
+
 
