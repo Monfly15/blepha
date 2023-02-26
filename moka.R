6c5eefdaeb448180a48247e3628c5ae583bfdca3
Monfly15 <126212044+Monfly15@users.noreply.github.com>
2023-02-26 13:57:15 -0500

    Update moka.R

--- moka.R
+++ moka.R
@@ -3,24 +3,24 @@ library(maps)
 library(phytools)
 library(mapdata)
 
-data <- read.csv("distribucion_ble.csv")[,1:3]
+data <- read.csv("distribucion_ble2.csv")[,1:3]
 data$spp[which(data$spp == "COL20_spn_ranulfoi" )] <- "COL020_spn_ranulfoi"
 
-tree <- read.nexus("blepha_final.nex")
+tree <- read.nexus("blepha_final2.nex")
 tree <- drop.tip(tree, "OUTGROUP")
+tree <- drop.tip(tree, "COL020_spn_ranulfoi")
 
 ##Necesitas una matriz
-data2 <- cbind(data$lat, data$long )
+data2 <- cbind(data$Lat, data$Long )
 row.names(data2) <- data[,1]
 
-##Ahora usas la funcion
-##Sospecho que tienes que ajustar la region.
-##Revisa este link 
-##http://blog.phytools.org/2019/03/a-follow-up-comment-on-phylotomap-for.html
-obj <- phylo.to.map(tree, data2,plot=FALSE, rotate=FALSE,
-                    database="worldHires")
-plot(obj)
+obj<-phylo.to.map(tree,data,database="worldHires",
+                  regions="Mexico",plot=FALSE,rotate=FALSE)
+cols<-setNames(sample(viridis(n=Ntip(tree))),
+               tree$tip.label)
 
+plot(obj,direction="rightwards",colors=cols,ftype="off",cex.points=c(0,1.5),
+     pts=FALSE)
 
 
 
