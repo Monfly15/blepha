843f467615e6f35f52216e5cf0646912195a44f1
Cristian Román-Palacios <cromanpa94@gmail.com>
2023-02-26 13:38:13 -0700

    name check

--- moka.R
+++ moka.R
@@ -1,11 +1,9 @@
-blepha_final2 <- read.table("~/MEGA/Doctorado/Semetre 7/Seminario VII/blepha_final2.nex", sep=";", quote="\"")
-distribucion_ble2 <- read.csv("~/MEGA/Doctorado/Semetre 7/Seminario VII/distribucion_ble2.csv")
-
-
 library(ape)
 library(maps)
 library(phytools)
 library(mapdata)
+library(viridis)
+library(geiger)
 
 data <- read.csv("distribucion_ble2.csv")[,1:3]
 data$spp[which(data$spp == "COL20_spn_ranulfoi" )] <- "COL020_spn_ranulfoi"
@@ -13,6 +11,12 @@ data$spp[which(data$spp == "COL20_spn_ranulfoi" )] <- "COL020_spn_ranulfoi"
 tree <- read.nexus("blepha_final2.nex")
 tree <- drop.tip(tree, "OUTGROUP")
 tree <- drop.tip(tree, "COL020_spn_ranulfoi")
+
+##Revisar que los datos del arbol y el set de datos coincidan
+#Debe decir Ok
+name.check(tree, data.names = data$spp)
+
+
 
 ##Necesitas una matriz
 data2 <- cbind(data$Lat, data$Long )
