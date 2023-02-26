fb18e01a37bd34a1518227837470be567295a2d3
Cristian Román-Palacios <cromanpa94@gmail.com>
2023-02-26 14:19:22 -0700

    Creando el mapa

--- moka.R
+++ moka.R
@@ -4,9 +4,22 @@ library(phytools)
 library(mapdata)
 library(viridis)
 library(geiger)
+library(ggplot2)
 
 data <- read.csv("distribucion_ble2.csv")[,1:3]
 data$spp[which(data$spp == "COL20_spn_ranulfoi" )] <- "COL020_spn_ranulfoi"
+data <- data[!data$spp == "COL020_spn_ranulfoi" ,]
+
+##Los puntos
+
+world_map <- map_data("world")
+
+ggplot() + 
+  geom_polygon(data=world_map, aes(x=long, y=lat, group=group), 
+               colour="light green", fill="light green") +
+  geom_point(data=data, 
+             aes(x=Lon, y=Lat), colour="Deep Pink", 
+             fill="Pink",pch=21, size=5, alpha=I(0.7))
 
 tree <- read.nexus("blepha_final2.nex")
 tree <- drop.tip(tree, "OUTGROUP")
@@ -16,10 +29,8 @@ tree <- drop.tip(tree, "COL020_spn_ranulfoi")
 #Debe decir Ok
 name.check(tree, data.names = data$spp)
 
-
-
 ##Necesitas una matriz
-data2 <- cbind(data$Lat, data$Long )
+data2 <- cbind(data$Lat, data$Lon)
 row.names(data2) <- data[,1]
 
 obj<-phylo.to.map(tree,data,database="worldHires",
